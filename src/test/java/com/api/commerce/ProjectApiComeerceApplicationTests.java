a04c0371e26a08a67b6725860acccb5de6ee477a
dariverap <08diegorivera@gmail.com>
2023-09-09 12:24:46 -0500

    Start project

---
+++ src/test/java/com/api/commerce/ProjectApiComeerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiComeerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
